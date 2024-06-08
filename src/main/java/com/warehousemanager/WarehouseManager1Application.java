02537b0ab051f57fe5088b0e3fc2a8e15b2437f5
Divyanagaraj21 <divyanagaraj2122@gmail.com>
2024-06-08 12:51:29 +0530

    warehouse_manager

---
+++ src/main/java/com/warehousemanager/WarehouseManager1Application.java
@@ -0,0 +1,13 @@
+package com.warehousemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseManager1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseManager1Application.class, args);
+	}
+
+}
